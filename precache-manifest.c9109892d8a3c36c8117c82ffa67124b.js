e52418902ddd5c2f2d69663e3f74e2b687b1b998
Варя Лиханина <varyasimac@iMac-Vara.local>
2020-10-16 01:06:58 +0300

    Updates

---
+++ precache-manifest.c9109892d8a3c36c8117c82ffa67124b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6a8d748b1495092c0a3ec783b6e670",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "625b4e7df72d86ec5c33",
+    "url": "/recipe-app/static/css/main.f826174d.chunk.css"
+  },
+  {
+    "revision": "bd7907e16c76f77988fe",
+    "url": "/recipe-app/static/js/2.9509b71d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recipe-app/static/js/2.9509b71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625b4e7df72d86ec5c33",
+    "url": "/recipe-app/static/js/main.6c545ae5.chunk.js"
+  },
+  {
+    "revision": "95f8ec566e7199ca77cb",
+    "url": "/recipe-app/static/js/runtime-main.5e04f524.js"
+  }
+]);
